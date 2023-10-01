597b34313a3899ab5fcc88562e5a3094005e4d58
Egon Saks <egonsaks@gmail.com>
2023-10-01 08:18:53 +0300

    adjust all accounts ui

--- src/system.c
+++ src/system.c
@@ -161,11 +161,13 @@ void checkAllAccounts(struct User u) {
   printf("\t\t====== All accounts from user, %s =====\n\n", u.name);
   while (getAccountFromFile(pf, userName, &r)) {
     if (strcmp(userName, u.name) == 0) {
-      printf("_____________________\n");
-      printf("\nAccount number:%d\nDeposit Date:%d/%d/%d \ncountry:%s \nPhone "
-             "number:%d \nAmount deposited: $%.2f \nType Of Account:%s\n",
-             r.accountNbr, r.deposit.day, r.deposit.month, r.deposit.year,
-             r.country, r.phone, r.amount, r.accountType);
+      printf("\t\t_____________________\n");
+      printf(
+          "\n\t\tAccount number: %d\n\t\tDeposit date: %d/%d/%d\n\t\tCountry: "
+          "%s \n\t\tPhone number: %d \n\t\tAmount deposited: $%.2f \n\t\tType "
+          "of account: %s\n",
+          r.accountNbr, r.deposit.day, r.deposit.month, r.deposit.year,
+          r.country, r.phone, r.amount, r.accountType);
     }
   }
   fclose(pf);
